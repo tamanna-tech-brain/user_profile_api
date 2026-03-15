29525ee93d7bc01cb09fe9082507470717350bce
Tamanna_verma <tamannaverma7389@gmail.com>
2026-03-15 12:33:36 +0530

    add all files

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ const userRouter = require('./routes/user');
 
 const app = express();
 const PORT = 8000;
-
+//app.use(express.json());
 //connection
 connectMongoDb('mongodb://127.0.0.1:27017/youtube-app-1').then(() => 
     console.log("Mongodb connected!")
 );
 
 //middleware-plugin
+app.use(express.json());
 app.use(express.urlencoded({ extended : false}));
 app.use(logReqRes("log.txt"));
 
